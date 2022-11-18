a72bbec4bab19d6b169dee5ce6dc7b7c1fa0670f
Hadleigh Richards <richardshgk@gmail.com>
2022-11-18 09:47:30 -0500

    fixed spelling error

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ plt.style.use("ggplot")
 
 # Create multiple plots 
 fig, (ax1, ax2) = plt.subplots(1,2)
-fig.suptitle('Closing Cases Using Forensic Genetic Geneology(FGG)')
+fig.suptitle('Closing Cases Using Forensic Genetic Genealogy(FGG)')
 
 # Units for bins
 bins1 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
